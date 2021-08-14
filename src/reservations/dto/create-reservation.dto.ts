9266114c704e7aa899dde65de265f1cf4d19637a
Rafael Peres <rafael.peres@allya.com.br>
2021-08-14 12:50:49 -0300

    added config in swagger

--- src/reservations/dto/create-reservation.dto.ts
+++ src/reservations/dto/create-reservation.dto.ts
@@ -1,8 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateReservationDto {
+  @ApiProperty()
   apartmentName: string;
+
+  @ApiProperty()
   dateCheckin: Date;
+
+  @ApiProperty()
   dateCheckout: Date;
+
+  @ApiProperty()
   numberGuests: number;
+
+  @ApiProperty()
   nameGuests: Array<string>;
+
+  @ApiProperty()
   guestEmail: string;
 }
